258616dbe46f029c28392166e489b53c494b40be
Kairu <albert850904@gmail.com>
2022-01-21 17:45:04 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ubi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ubitus-slider';
+}
